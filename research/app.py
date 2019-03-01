d633e52d0ee38160c232e1ecf39ebb767531450e
Prosperity Paul <prosperpaul@ymail.com>
2019-03-01 19:37:22 +0300

    Update app.py

--- research/app.py
+++ research/app.py
@@ -20,47 +20,47 @@ app = Flask(__name__)
 
 
 def login():
-    if not session.get('logged_in'):
-        form = forms.LoginForm(request.form)
-        if request.method == 'POST':
-            username = request.form['username'].lower()
-            password = request.form['password']
-            if form.validate():
-                if helpers.credentials_valid(username, password):
-                    session['logged_in'] = True
-                    session['username'] = username
-                    return json.dumps({'status': 'Login successful'})
-                return json.dumps({'status': 'Invalid user/pass'})
-            return json.dumps({'status': 'Both fields required'})
-        return render_template('login.html', form=form)
-    user = helpers.get_user()
-    return render_template('home.html', user=user)
+   # if not session.get('logged_in'):
+     #   form = forms.LoginForm(request.form)
+        #if request.method == 'POST':
+           # username = request.form['username'].lower()
+            #password = request.form['password']
+           # if form.validate():
+                #if helpers.credentials_valid(username, password):
+                    #session['logged_in'] = True
+                    #session['username'] = username
+                    #return json.dumps({'status': 'Login successful'})
+               # return json.dumps({'status': 'Invalid user/pass'})
+            #return json.dumps({'status': 'Both fields required'})
+        #return render_template('login.html', form=form)
+    #user = helpers.get_user()
+    return render_template('home.html')
 
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False
-    return redirect(url_for('login'))
+#@app.route("/logout")
+#def logout():
+    #session['logged_in'] = False
+    #return redirect(url_for('login'))
 
 
 # -------- Signup ---------------------------------------------------------- #
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if not session.get('logged_in'):
-        form = forms.LoginForm(request.form)
-        if request.method == 'POST':
-            username = request.form['username'].lower()
-            password = helpers.hash_password(request.form['password'])
-            email = request.form['email']
-            if form.validate():
-                if not helpers.username_taken(username):
-                    helpers.add_user(username, password, email)
-                    session['logged_in'] = True
-                    session['username'] = username
-                    return json.dumps({'status': 'Signup successful'})
-                return json.dumps({'status': 'Username taken'})
-            return json.dumps({'status': 'User/Pass required'})
-        return render_template('login.html', form=form)
-    return redirect(url_for('login'))
+#@app.route('/signup', methods=['GET', 'POST'])
+#def signup():
+   # if not session.get('logged_in'):
+       # form = forms.LoginForm(request.form)
+        #if request.method == 'POST':
+           # username = request.form['username'].lower()
+           # password = helpers.hash_password(request.form['password'])
+            #email = request.form['email']
+            #if form.validate():
+                #if not helpers.username_taken(username):
+                   # helpers.add_user(username, password, email)
+                    #session['logged_in'] = True
+                    #session['username'] = username
+                   # return json.dumps({'status': 'Signup successful'})
+                #return json.dumps({'status': 'Username taken'})
+           # return json.dumps({'status': 'User/Pass required'})
+       # return render_template('login.html', form=form)
+    #return redirect(url_for('login'))
 
 @app.route('/prediction', methods=['GET', 'POST'])
 def prediction():
