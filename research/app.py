5f0a763532df325f3dd1842ca88f45b561e5e31a
Prosperity Paul <prosperpaul@ymail.com>
2019-04-01 10:27:52 +0300

    changes

--- research/app.py
+++ research/app.py
@@ -3,7 +3,7 @@
 from scripts import tabledef
 from scripts import forms
 from scripts import helpers
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session,abort
 import json
 import sys
 import os
@@ -25,16 +25,16 @@ def login():
         if request.method == 'POST':
             username = request.form['username'].lower()
             password = request.form['password']
-            if form.validate():
-                if helpers.credentials_valid(username, password):
-                    session['logged_in'] = True
-                    session['username'] = username
-                    return json.dumps({'status': 'Login successful'})
-                return json.dumps({'status': 'Invalid user/pass'})
-            return json.dumps({'status': 'Both fields required'})
+           # if form.validate():
+                #if helpers.credentials_valid(username, password):
+                   # session['logged_in'] = True
+                    #session['username'] = username
+                   # return json.dumps({'status': 'Login successful'})
+               # return json.dumps({'status': 'Invalid user/pass'})
+            #return json.dumps({'status': 'Both fields required'})
         return render_template('login.html', form=form)
-    user = helpers.get_user()
-    return render_template('home.html', user=user)
+   # user = helpers.get_user()
+    return render_template('home.html')
 
 @app.route("/logout")
 def logout():
