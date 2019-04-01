b3283fa95d567315d6fc4bf477d41aee1dfdcec5
Prosperity Paul <prosperpaul@ymail.com>
2019-04-01 12:22:20 +0300

    changes

--- research/scripts/tabledef.py
+++ research/scripts/tabledef.py
@@ -1,34 +1,26 @@
-# -*- coding: utf-8 -*-
-
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+rom sqlalchemy import *
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///accounts.db'
-
+from sqlalchemy.orm import relationship, backref
+ 
+engine = create_engine('sqlite:///tutorial.db', echo=True)
 Base = declarative_base()
-
-
-def db_connect():
-    """
-    Performs database connection using database settings from settings.py.
-    Returns sqlalchemy engine instance
-    """
-    return create_engine(SQLALCHEMY_DATABASE_URI)
-
-
+ 
+########################################################################
 class User(Base):
-    __tablename__ = "user"
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String(30), unique=True)
-    password = Column(String(30))
-    email = Column(String(50))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
-engine = db_connect()  # Connect to database
-Base.metadata.create_all(engine)  # Create models
+""""""
+__tablename__ = "users"
+ 
+id = Column(Integer, primary_key=True)
+username = Column(String)
+password = Column(String)
+ 
+#----------------------------------------------------------------------
+def __init__(self, username, password):
+""""""
+self.username = username
+self.password = password
+ 
+# create tables
+Base.metadata.create_all(engine)
